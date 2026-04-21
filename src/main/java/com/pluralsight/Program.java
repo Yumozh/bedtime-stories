1f60466b41e45749207751629a063274da8537eb
Yumozh <juliiamozhchil@gmail.com>
2026-04-21 10:14:39 -0500

    Request info from user

--- src/main/java/com/pluralsight/Program.java
+++ src/main/java/com/pluralsight/Program.java
@@ -1,4 +1,18 @@
 package com.pluralsight;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.Scanner;
+
 public class Program {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("What story do you wan to read today? ");
+        String storyName = scan.nextLine();
+
+//        FileReader fileReader = new FileReader("goldilocks.txt");
+//        BufferedReader bufferedReader = new BufferedReader(fileReader);
+//
+//        String input = bufferedReader.readLine();
+    }
 }
