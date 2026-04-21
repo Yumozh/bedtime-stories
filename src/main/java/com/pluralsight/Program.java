83979b1e5f8e4ffbcf20f76cf198effa7e84e323
Yumozh <juliiamozhchil@gmail.com>
2026-04-21 10:23:06 -0500

    feat: read selected story

--- src/main/java/com/pluralsight/Program.java
+++ src/main/java/com/pluralsight/Program.java
@@ -2,6 +2,7 @@ package com.pluralsight;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.io.IOException;
 import java.util.Scanner;
 
 public class Program {
@@ -10,9 +11,23 @@ public class Program {
         System.out.println("What story do you wan to read today? ");
         String storyName = scan.nextLine();
 
-//        FileReader fileReader = new FileReader("goldilocks.txt");
-//        BufferedReader bufferedReader = new BufferedReader(fileReader);
-//
-//        String input = bufferedReader.readLine();
+
+        try {
+            FileReader fileReader = new FileReader("goldilocks.txt");
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+
+            String input = bufferedReader.readLine();
+            int i = 1;
+            while(input != null){
+                System.out.println(i+"." + input);
+                input = bufferedReader.readLine();
+                i ++;
+            }
+
+            bufferedReader.close();
+        } catch (IOException e) {
+            System.out.println("The file could not be read. Please make sure the file is available and not locked and then try again.");
+            throw new RuntimeException(e);
+        }
     }
 }
